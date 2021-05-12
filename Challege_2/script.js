5d7f1ddd4936c5093bafd4ef7028c4c1bff91dc1
felixandrewsapalaran <felixandrewsapalaran@gmail.com>
2021-05-12 15:28:18 -0400

    Convert Minutes to Seconds

---
+++ Challege_2/script.js
@@ -0,0 +1,13 @@
+/*
+Convert Minutes to Seconds
+*/
+
+function convertMinutestoSeconds(minutes) {
+    seconds = minutes * 60;
+    return seconds
+}
+
+console.log(convertMinutestoSeconds(1));
+console.log(convertMinutestoSeconds(2));
+console.log(convertMinutestoSeconds(3));
+ 
