30052c9db06ba5533813667b1477dd79eff986af
felixandrewsapalaran <felixandrewsapalaran@gmail.com>
2021-05-12 15:51:54 -0400

    Your age in Seconds

---
+++ Challenge_3/ageToSeconds.js
@@ -0,0 +1,11 @@
+/*
+Converting your age into seconds
+*/
+
+function ageToSeconds(age){
+    numberOfSecondsInYear = 3.154e+7;
+    result = age * numberOfSecondsInYear;
+    return result
+}
+
+console.log(ageToSeconds(25));
